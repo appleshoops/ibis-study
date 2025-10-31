06b08e446ac70a0e5278a0cb85cbcb8fa202a012
appleshoops <95598768+appleshoops@users.noreply.github.com>
2025-10-31 12:40:54 +1100

    Added register capabilities

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 def index():
@@ -17,6 +23,17 @@ def login():
 
 @app.route('/register')
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        displayName = request.form['displayName']
+        email = request.form['email']
+        password = request.form['password']
+        conformPassword = request.form['confirmPassword']
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        exists = cursor.execute(f"SELECT COUNT(username) FROM Users WHERE username = '{username}'")
     return render_template('register.html')
 
 if __name__ == '__main__':
